fff4428bad496ab494504966985d6a92b5b4e3e8
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-12 23:57:12 +0200

    Initial commit

---
+++ sample/src/main/kotlin/com/ahmedmourad/mirror/sample/Main.kt
@@ -0,0 +1,11 @@
+package com.ahmedmourad.mirror.sample
+
+data class User private constructor(
+        val name: String,
+        val email: String,
+        val phoneNumber: String
+)
+
+fun main() {
+
+}
